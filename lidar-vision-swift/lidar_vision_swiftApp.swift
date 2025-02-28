bbfbad56ccf961aebc40afaafa5ec37db716cc7f
tmba <ut42.nu@gmail.com>
2025-02-28 16:07:44 +0530

    Initial Commit

---
+++ lidar-vision-swift/lidar_vision_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  lidar_vision_swiftApp.swift
+//  lidar-vision-swift
+//
+//  Created by Takuya Uehara on 2025/02/28.
+//
+
+import SwiftUI
+
+@main
+struct lidar_vision_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
